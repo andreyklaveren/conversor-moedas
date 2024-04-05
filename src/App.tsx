ceb9ebd906b6ba89fefda1ad9a25e2fb3102fcd2
andreyklaveren <andreyklaveren@gmail.com>
2024-04-05 12:23:16 -0300

    adicionado background image na página e seção de informações de legenda

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Header } from "./components/Header";
 import { CurrencyConverterForm } from "./components/form/CurrencyConverterForm";
-
+import bg from "./assets/bg.svg";
 export function App() {
   return (
-    <div className="p-16">
+    <div
+      className="p-16 bg-cover bg-right bg-no-repeat"
+      style={{
+        backgroundImage: `url(${bg})`,
+        height: "100vh",
+        backgroundSize: "contain",
+      }}
+    >
       <Header />
+
       <CurrencyConverterForm />
     </div>
   );
--- src/components/form/CurrencyConverterForm.tsx
+++ src/components/form/CurrencyConverterForm.tsx
@@ -5,60 +5,74 @@ import { api } from "../../services/api";
 import { useEffect, useState } from "react";
 
 export function CurrencyConverterForm() {
-  const [cotacaoDolar, setCotacaoDolar] = useState(0);
-  const [valorUsuario, setValorUsuario] = useState(0);
+  const [convertedValueUSD_to_BRL, setConvertedValueUSD_to_BRL] = useState("");
+  const [userValue, setUserValue] = useState(0);
   const [result, setResult] = useState(0);
 
   useEffect(() => {
-    api.get("USD-BRL").then((response) => {
-      const cotacao = parseFloat(response.data.USDBRL.ask);
-      if (!isNaN(cotacao)) {
-        setCotacaoDolar(cotacao.toFixed(2));
-      } else {
-        console.error("O valor da cotação do dólar não é um número.");
-      }
-    });
-  }, [cotacaoDolar]);
+    api
+      .get("USD-BRL")
+      .then((response) => {
+        setConvertedValueUSD_to_BRL(
+          parseFloat(response.data.USDBRL.ask).toFixed(2) // Pega o valor convertido de USD to BRL
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [convertedValueUSD_to_BRL]);
 
-  const handleSetValor = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValorUsuario(parseInt(event.target.value));
+  const handleSetValor = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setUserValue(parseFloat(value));
   };
 
   function calculate(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
-    setResult(cotacaoDolar * valorUsuario);
+    setResult(parseFloat(convertedValueUSD_to_BRL) * userValue);
   }
 
   return (
-    <form className="mt-28">
-      <section className="flex gap-6">
-        <InputData
-          onChange={handleSetValor}
-          nome="Dólar"
-          placeholder="$ 1,00"
-        />
-        <InputData nome="Taxa do estado" placeholder="%" />
-      </section>
-      <section className="mt-8">
-        <h2 className="text-lg mb-4">Tipo de compra</h2>
-        <div className="flex gap-4">
-          <InputRadio name="Dinheiro" metodoPagamento="Dinheiro" />
-          <InputRadio name="Cartao" metodoPagamento="Cartão" />
+    <div>
+      <form className="mt-28 mb-28">
+        <section className="flex gap-6">
+          <InputData
+            onChange={handleSetValor}
+            nome="Dólar"
+            placeholder="$ 1,00"
+          />
+          <InputData nome="Taxa do estado" placeholder="%" />
+        </section>
+        <section className="mt-8">
+          <h2 className="text-lg mb-4">Tipo de compra</h2>
+          <div className="flex gap-4">
+            <InputRadio name="Dinheiro" metodoPagamento="Dinheiro" />
+            <InputRadio name="Cartao" metodoPagamento="Cartão" />
+          </div>
+        </section>
+        <ConvertBtn onClick={calculate} />
+      </form>
+      <div className="flex flex-col gap-1">
+        <div className="max-w-[90px]">
+          <p className="text-xl">Informações</p>
         </div>
-      </section>
-      <ConvertBtn onClick={calculate} />
-      {
-        <div>
-          <section>
-            Cotação do dólar:
-            {cotacaoDolar}
+        {
+          <section className="flex flex-col gap-1 p-4 max-w-[360px] border-2 rounded-lg bg-[#00AB63] border-[#008B57] drop-shadow-2xl">
+            <div>
+              <p className="text-white">
+                <strong>Cotação do dólar:</strong> $1,00 = R$
+                {convertedValueUSD_to_BRL}
+              </p>
+            </div>
+            <div>
+              <p className="text-white">
+                <strong>O resultado do cálculo é</strong> R${result.toFixed(2)}
+              </p>
+            </div>
           </section>
-          <section>
-            Cotação do dólar:
-            {result}
-          </section>
-        </div>
-      }
-    </form>
+        }
+      </div>
+    </div>
   );
 }
